e7eaa942b337bc7ed7d341eecda7430239821757
emanuelvs <emanuelvisantos99@gmail.com>
2023-12-01 21:19:22 -0300

    [0.0.1] Start project

---
+++ src/main/java/br/ufba/schedulingsimulatorso/SchedulingSimulatorSoApplication.java
@@ -0,0 +1,13 @@
+package br.ufba.schedulingsimulatorso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingSimulatorSoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingSimulatorSoApplication.class, args);
+    }
+
+}
